0e89fffdf92c959a0ce606f26edc98ff0240e57a
mohamed.tayel <moh.moh701@gmail.com>
2021-08-21 22:26:15 +0200

    add shared module with ngPrime controls

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {AccordionModule} from 'primeng/accordion';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AccordionModule,
+    InputTextModule,
+    ButtonModule,
+    DropdownModule,   
+    TableModule,
+    MessagesModule,
+    MessageModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    CommonModule,
+    AccordionModule,
+    InputTextModule,
+    ButtonModule,
+    DropdownModule,   
+    TableModule,
+    MessagesModule,
+    MessageModule,
+    BrowserAnimationsModule
+  ]
+})
+export class SharedModule { }
